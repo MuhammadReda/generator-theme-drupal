578a5d2ba6445299337ee08b1ca64f67b6077216
Muhammad Reda <say.hello@mreda.net>
2014-08-21 18:34:09 +0300

    Added comments.

--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,7 @@
 var filterThemeName = function(input) {
     var output = input.toLowerCase();
-    output = output.replace(/^[0-9\\.\\s]+/, '');
-    output = output.replace(/[^a-zA-Z0-9]/g, '_');
+    output = output.replace(/^[0-9\\.\\s]+/, ''); // replace numbers at the beginning with nothing.
+    output = output.replace(/[^a-zA-Z0-9]/g, '_'); // replace special characters with _
     return output;
 }
 
