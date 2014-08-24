38be92cc3f755beb4e8d5e1e798fe59598f9b7b7
Muhammad Reda <say.hello@mreda.net>
2014-08-24 13:56:16 +0300

    Added new function definition 'extractArrayIndexes'

--- utils/utils.js
+++ utils/utils.js
@@ -1,8 +1,27 @@
+var _ = require('lodash');
+
+/**
+*
+*/
 var filterThemeName = function(input) {
     var output = input.toLowerCase();
     output = output.replace(/^[0-9\\.\\s]+/, ''); // replace numbers at the beginning with nothing.
     output = output.replace(/[^a-zA-Z0-9]/g, '_'); // replace special characters with _
     return output;
+};
+
+
+/**
+*
+*/
+var extractArrayIndexes = function(input) {
+    var output = {};
+    _.forEach(input, function(item, index) {
+        output[item] = item;
+    });
+    return output;
 }
 
+
 exports.filterThemeName = filterThemeName;
+exports.extractArrayIndexes = extractArrayIndexes;
