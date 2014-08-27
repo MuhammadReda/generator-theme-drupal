7bc6ea3b77efb88534931b85758008f2cf54f70d
Muhammad Reda <say.hello@mreda.net>
2014-08-27 14:14:40 +0300

    Fixed a bug in variable names.

--- app/templates/d7/_template.php
+++ app/templates/d7/_template.php
@@ -5,7 +5,7 @@
 /**
 * theme_preprocess_html()
 */
-function <%= themeNameSanitized %>_preprocess_html(&$vars) {
+function <%= themeName %>_preprocess_html(&$vars) {
 
     $vars['add_responsive_tags'] = true;
 }
@@ -15,10 +15,10 @@ function <%= themeNameSanitized %>_preprocess_html(&$vars) {
 /**
 * Implementation of hook_js_alter()
 */
-function <%= themeNameSanitized %>_js_alter(&$js) {
+function <%= themeName %>_js_alter(&$js) {
 
     // add modernizr
-    $modernizrKey = "<%= themeNameSanitized %>_modernizr";
+    $modernizrKey = "<%= themeName %>_modernizr";
     $js[$modernizrKey] = $js['misc/drupal.js'];
     $js[$modernizrKey]['data'] = '//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.min.js';
     $js[$modernizrKey]['weight'] = -999;
